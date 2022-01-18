f0eda20e09acffc7dba417f68ca878f57365f00a
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-18 12:34:54 -0500

    accessing with refs wizardry

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -1,26 +1,49 @@
 import { useState } from "react";
 import React from "react";
 import { StyleSheet, Button, Text, View } from "react-native";
-import VideoFF from "./VideoFF";
 import { Video, AVPlaybackStatus } from "expo-av";
 import { useAssets } from "expo-asset";
 
-export default function Buttons(props) {
+export default function Buttons() {
   const [isFloppaFriday, setIsFloppaFriday] = useState(false);
   const [doomer, setDoomer] = useState(false);
 
   const video = React.useRef(null);
-  const status = VideoFF.status;
+  const videoDoomer = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      <VideoFF status={status}/>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={require("../assets/floppafriday.mp4")}
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <Video
+        ref={videoDoomer}
+        style={styles.video}
+        source={require("../assets/floppadoomer.mp4")}
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
       <View style={styles.buttons}>
         <Button
-          title={status.isPlaying ? "Pause" : "Play"}
+          title={status.isPlaying ? "PAUSE" : "PLAY"}
           onPress={() =>
             status.isPlaying
               ? video.current.pauseAsync()
               : video.current.playAsync()
+          }
+        />
+        <Button
+          title={status.isPlaying ? "PAUSE DOOMER" : "DOOMER"}
+          onPress={() =>
+            status.isPlaying
+              ? videoDoomer.current.pauseAsync()
+              : videoDoomer.current.playAsync()
           }
         />
       </View>
