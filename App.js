ebf56e6b0a3c4fc4e7f3c8e6daa6fb327b445d79
hstrejoluna <trejolunatutoriales@gmail.com>
2022-01-19 12:22:02 -0500

    friday logic done

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Buttons from './components/Buttons';
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Buttons from "./components/Buttons";
 
 export default function App() {
+  const [isFriday, setIsFriday] = useState(false);
+
+  useEffect(() => {
+    const today = new Date().getDay();
+    if (today === 5) {
+      setIsFriday(true);
+    }
+  }, []);
 
   
 
   return (
-    
     <View style={styles.container}>
-      <Buttons />
+      {isFriday ? <Buttons /> : <Text style={styles.text}>It's not Friday yet!</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -19,8 +26,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text:{
+    color: "#fff",
+    fontSize: 30,
+  }
 });
